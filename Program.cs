db86eaacfc5129026fadf961eb80ad436dc4eab4
Albertus06 <118900292+Albertus06@users.noreply.github.com>
2022-12-12 13:19:35 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.ReadLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bienvenido jugador a Adivinar el número");
+Console.WriteLine("Te explico la reglas:");
+Console.WriteLine("1. Tienes que adivinar el número del 1 al 15.");
+Console.WriteLine("2. Solo tienes 3 vidas.");
+var generadorAleatorio = new Random();
+int numeroParaAdivinar = generadorAleatorio.Next(1,15); 
+int i = 0;
+
+
+while (i < 3){
+    Console.WriteLine("Ingresa un número");
+    int respuesta = Convert.ToInt32(Console.ReadLine());
+    if (respuesta == numeroParaAdivinar){
+        Console.WriteLine($"Felicidades, acertaste el número en {i} intentos.");
+        break;
+    }
+    else{
+        Console.WriteLine($"Has fallado. Te quedan {2-i} intentos.");
+    i = i+1;
+        if (respuesta < numeroParaAdivinar){
+           Console.WriteLine("Tu número es más bajo") 
+        }
+    }
+    
+}
