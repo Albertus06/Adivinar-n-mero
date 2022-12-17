6feea2e4c0564d00e265f95cd45edcdac0a56583
Albertus06 <118900292+Albertus06@users.noreply.github.com>
2022-12-17 17:54:53 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,20 +7,31 @@ var generadorAleatorio = new Random();
 int numeroParaAdivinar = generadorAleatorio.Next(1,15); 
 int i = 0;
 
-
 while (i < 3){
     Console.WriteLine("Ingresa un número");
     int respuesta = Convert.ToInt32(Console.ReadLine());
-    if (respuesta == numeroParaAdivinar){
-        Console.WriteLine($"Felicidades, acertaste el número en {i} intentos.");
-        break;
-    }
-    else{
-        Console.WriteLine($"Has fallado. Te quedan {2-i} intentos.");
-    i = i+1;
-        if (respuesta < numeroParaAdivinar){
-           Console.WriteLine("Tu número es más bajo") 
+    if (respuesta <= 15 && respuesta > 0){
+        if (respuesta == numeroParaAdivinar){
+            Console.WriteLine($"Felicidades, acertaste el número en {i} intentos.");
+            break;
         }
-    }
-    
+        else{
+            Console.WriteLine($"Has fallado. Te quedan {2-i} intentos.");
+            if (respuesta < numeroParaAdivinar){
+                Console.WriteLine("Tu número es más bajo del que estoy pensando, vuelve a intentarlo.");
+            }
+            if (respuesta > numeroParaAdivinar){
+                Console.WriteLine("Tu número es más alto del que estoy pensando, vuelve a intentarlo.");
+            }
+            i = i+1;
+            if (i > 3){
+                Console.WriteLine("Perdiste, te has quedado sin vidas.");
+                Console.WriteLine($"El número que estaba pensando era el {numeroParaAdivinar}.");
+                
+                break;
+            } 
+     }
+     else{
+        Connsole.Writeline("Error, vuelve a ingresar el número.");
+     }
 }
